86497e5c190c1c2aeab5649584747310e8c93d24
Meamya <meamyac@yahoo.com>
2017-04-17 19:02:34 -0600

    make folders

---
+++ server.js
@@ -0,0 +1,37 @@
+//section 1 npm download packages
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+var config = require("./config");
+var logger = require("morgan");
+//var expressJwt = require("express-jwt")
+var port = process.env.PORT || 8000;
+
+//section 2 
+app.use(bodyParser.json());
+//app.use(express.static(path.join(__dirname, "public")))
+app.use(logger("dev"));
+
+//connect to database 
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database, function (err) {
+	if (err) throw err;
+	console.log("Successully connected to the database")
+});
+
+//app uses these apis and connects to these routes
+//app.use("/api", expressJwt({
+//	secret: config.secret
+//}));
+
+//app.use("/api/profile", require("./routes/profileRoutes"));
+//app.use("/api/chakra", require("./routes/chakraRoutes"));
+//app.use("/auth", require("./routes/authRoutes"));
+//app.use("/search", require("./routes/searchRoutes"));
+
+//app is listening at this port
+app.listen(port, function () {
+	console.log("Server is listening on port: " + port)
+});
