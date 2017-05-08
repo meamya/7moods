9803d6596b357ea2bf96cd362e2e966f20444d68
Meamya <meamyac@yahoo.com>
2017-05-08 09:58:01 -0600

    terminal

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    database:"mongodb://localhost/chakra",
+module.exports = {require('dotenv').config()
+    database: process.env.MONGOHQ_URL || "mongodb://localhost:27017/chakra",
                 }
